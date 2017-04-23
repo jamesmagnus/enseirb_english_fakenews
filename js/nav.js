13f62aaf94c9cbf0627abb6cb2d84aa582689be2
Jean-Loup Beaussart <jbeaussart@enseirb-matmeca.fr>
2017-04-23 18:58:12 +0200

    up

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,16 @@
 $(function(){
     $("#include-header").load("header.html");
-    $("#include-nav").load("nav.html");
+    $("#include-nav").load("nav.html", active_nav);
     $("#include-footer").load("footer.html");
 });
 
-$(function() {
-     var pgurl = window.location.href.substr(window.location.href
-.lastIndexOf("/")+1);
-     $("nav ul li").each(function(){
-         if($(this).children().attr("href") == pgurl || $(this).children().attr("href") == '' )
-          $(this).addClass("active");
-     })
-});
+function active_nav() {
+    var pgurl = window.location.href.substr(window.location.href
+                                            .lastIndexOf("/")+1);
+
+    $("nav ul li").each(function(){
+        if($(this).children().attr("href") == pgurl || $(this).children().attr("href") == '' )
+            $(this).addClass("active");
+    })
+        }
 
